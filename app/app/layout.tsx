b1d3903a2b9b74811bc1d90162b9b52730db83d5
Johnson Nifemi <johnsonnifemi52@gmail.com>
2025-11-11 16:22:27 +0100

    minor fix

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,26 +1,22 @@
+import type React from "react"
 import { SidebarLeft } from "@/components/sidebar-left"
 import { SidebarRight } from "@/components/sidebar-right"
 import { DynamicHeader } from "@/components/dynamic-header"
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
 export default function ProtectedLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <SidebarProvider className="flex flex-row">
+    <SidebarProvider>
       <SidebarLeft />
-      <SidebarInset className="flex flex-col min-w-0 flex-1">
+      <SidebarInset>
         <DynamicHeader />
-        <div className="flex flex-1 flex-col gap-4 p-4 overflow-auto">
-          {children}
-        </div>
+        <div className="flex flex-1 flex-col gap-4 p-4 overflow-auto max-w-full">{children}</div>
       </SidebarInset>
       <SidebarRight />
     </SidebarProvider>
-  );
+  )
 }
